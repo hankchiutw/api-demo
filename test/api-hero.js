b079034ef0f43d7f5962debe55476e56c58662fb
Hank Chiu <hankchiu.tw@gmail.com>
2016-12-14 07:48:43 +0800

    add api testing: /heroes

--- test/api-hero.js
+++ test/api-hero.js
@@ -1,14 +1,28 @@
 'use strict';
 
-const apiUrl = require('config/config').publicApiUrl;
+const rp = require('request-promise');
 const assert = require('chai').assert;
+
+const apiUrl = require('config/config').publicApiUrl;
 const api = require('chai-api')(apiUrl);
 
+const mockUrl = require('config/config').mockUrl;
+const mockApi = require('chai-api')(mockUrl);
+
 /** hero api tests */
-module.exports = function(mock){
+module.exports = function(){
     describe(`>>${__filename}`, function(){
+        let mock = {};
+
+        before(function*(){
+            mock.heroes = yield mockApi('GET', '/heroes');
+        });
+
         // req object for each tests
         beforeEach(function(){
+            this.authHeaderOk = { name: 'hahow', password: 'rocks' };
+            this.authHeaderFail = { name: 'hahow', password: 'rockss' };
+            this.authHeaderIgnore = { name1: 'hahow', password: 'rocks' };
         });
 
         // remove created data
@@ -20,9 +34,20 @@ module.exports = function(mock){
          */
 
         describe('#list', function(){
-            it('should successfully list', function*(){
+            it('should get public list', function*(){
                 const req = {};
-                yield api.success('GET', '/heroes', req);
+                const ret = yield api('GET', '/heroes', req);
+                assert.isOk(ret, 'results should be defined');
+
+                assert.deepEqual(ret, mock.heroes, 'results should equal to mock data');
+            });
+        });
+
+        describe.skip('#secret list', function(){
+            it('should get secret list', function*(){
+                const req = { headers: this.authHeaderOk };
+                const ret = yield api('GET', '/heroes', req);
+                assert.isOk(ret, 'results should be defined');
             });
         });
 
--- test/api.spec.js
+++ test/api.spec.js
@@ -4,18 +4,20 @@ require('mocha-generators').install();
 
 const apiUrl = require('config/config').publicApiUrl;
 const assert = require('chai').assert;
+
 const api = require('chai-api')(apiUrl);
 
 /** api tests */
 describe(`>>${__filename}`, function(){
-
-    it('API service ok', function*(){
-        yield api.success('GET', '/hello');
+    it('API service should be ok', function*(){
+        const ret = yield api('GET', '/hello');
+        assert.isOk(ret, 'result should be defined');
     });
 
     /**
      * sub tests
      */
+
     require('./api-hero')();
 
 });
