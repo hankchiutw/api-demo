deb15881941d276fd97e9e61a8162f3436a821bc
Hank Chiu <hankchiu.tw@gmail.com>
2016-12-14 09:20:29 +0800

    add testing for all APIs

--- test/api-hero.js
+++ test/api-hero.js
@@ -16,6 +16,9 @@ module.exports = function(){
 
         before(function*(){
             mock.heroes = yield mockApi('GET', '/heroes');
+            assert.isArray(mock.heroes, 'heroes mock should be an array');
+
+            mock.randomId = Math.ceil(Math.random()*mock.heroes.length);
         });
 
         // req object for each tests
@@ -37,17 +40,57 @@ module.exports = function(){
             it('should get public list', function*(){
                 const req = {};
                 const ret = yield api('GET', '/heroes', req);
-                assert.isOk(ret, 'results should be defined');
+                assert.isArray(ret, 'results should be an array');
 
                 assert.deepEqual(ret, mock.heroes, 'results should equal to mock data');
             });
         });
 
-        describe.skip('#secret list', function(){
+        describe('#secret list', function(){
             it('should get secret list', function*(){
+                this.timeout(3000);
                 const req = { headers: this.authHeaderOk };
                 const ret = yield api('GET', '/heroes', req);
-                assert.isOk(ret, 'results should be defined');
+                assert.isArray(ret, 'results should be an array');
+
+                ret.forEach(item => {
+                    assert.property(item, 'profile', 'profile property should be defined');
+                });
+            });
+        });
+
+        describe('#one', function(){
+            it('should get one public item', function*(){
+                const req = {};
+                const ret = yield api('GET', `/heroes/${mock.randomId}`, req);
+                assert.isOk(ret.id, 'id should be defined');
+            });
+        });
+
+        describe('#secret one', function(){
+            it('should get one secret item', function*(){
+                this.timeout(3000);
+                const req = { headers: this.authHeaderOk };
+                const ret = yield api('GET', `/heroes/${mock.randomId}`, req);
+                assert.isOk(ret.id, 'id should be defined');
+                assert.property(ret, 'profile', 'profile property should be defined');
+            });
+        });
+
+        describe('#unauthorized request', function(){
+            it('should get status code 401', function*(){
+                this.timeout(5000);
+                const req = {
+                    method: 'GET',
+                    uri: `${apiUrl}/heroes`,
+                    headers: this.authHeaderFail
+                };
+                yield rp(req).then( res => {
+                    assert.isOk(false, 'should not get response');
+                }, res => {
+                    assert.isOk(res, 'should have error response');
+                    assert.equal(res.statusCode, 401, 'should have response 401');
+                });
             });
         });
 
